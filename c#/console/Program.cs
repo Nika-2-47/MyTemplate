61d90d99ddcc67b0ac0719364d332993c983e462
Nika <tortoise.2473@gmail.com>
2025-09-22 19:28:23 +0900

    Add .net console app template

---
+++ c#/console/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+// ロガーファクトリを直接作る
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddConsole()
+        .AddDebug();
+});
+
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogInformation("一度だけログを出力します");
+
+// ここでプログラム終了
+Console.WriteLine("処理終了");
